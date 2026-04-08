eda651f863584d74a59203b0330a8bc61546e87a
KATTA SOWMYA <2420080041@klh.edu.in>
2026-04-08 21:03:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+    <h2>Prediction Result:</h2>
+    <h1>{{ prediction_text }}</h1>
+
+    <a href="/">Go Back</a>
+</body>
+</html>
